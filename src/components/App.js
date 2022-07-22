f90075cf4f71db6bce162ed85e35532d041d5507
Sarah Mukuti <cesarwrites24@gmail.com>
2022-07-22 23:51:58 +0300

    commit tree 0351fc22da1020fe7c97b229923164d0e3f106ca

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,17 @@ import React, {useState, useEffect} from "react";
 
 function App(){
     const [dogPic, setDogPic] = useState(null);
-
-    useEffect(() => {
+    const fetchData =()=> {
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(response=>response.json())
         .then(data => {
             setDogPic(data.message);
-        });
-    }, []);
+});
+    }
+      
+    useEffect(() => {
+        fetchData();
+}, []);
 
     if (!dogPic) 
     return (
